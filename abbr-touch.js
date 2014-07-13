8c308e6e6d79e4d7a38e87467b6fabb9af19f5ca
Tyriar <tyriar@tyriar.com>
2014-07-13 08:03:35 -0700

    Factor out tap event

--- abbr-touch.js
+++ abbr-touch.js
@@ -3,66 +3,15 @@
 var abbrTouch = (function () {
   'use strict';
 
-  var isTapLength;
-  var tapLengthTimeout;
-  var startPosition   = { x: -1, y: -1 };
-  var currentPosition = { x: -1, y: -1 };
-
   function init () {
     var elements = document.querySelectorAll('abbr[title]');
     for (var i = 0; i < elements.length; i++) {
-      elements[i].addEventListener('touchstart', touchstart);
-      elements[i].addEventListener('touchend', touchend);
-      elements[i].addEventListener('touchcancel', touchend);
-      elements[i].addEventListener('touchmove', touchmove);
+      elements[i].addEventListener('touchtap', tap);
     }
   }
 
-  function touchstart(e) {
-    var pointerEvent = getPointerEvent(e);
-    startPosition.x = pointerEvent.pageX;
-    startPosition.y = pointerEvent.pageY;
-    currentPosition.x = pointerEvent.pageX;
-    currentPosition.y = pointerEvent.pageY;
-    isTapLength = true;
-    if (tapLengthTimeout) {
-      clearTimeout(tapLengthTimeout);
-    }
-    tapLengthTimeout = setTimeout(function () {
-      isTapLength = false;
-    }, 200);
-  }
-
-  function touchend(e) {
-    if (isTapLength &&
-        approximatelyEqual(startPosition.x, currentPosition.x) &&
-        approximatelyEqual(startPosition.y, currentPosition.y)) {
-      tap(e.target);
-    }
-  }
-
-  function touchmove(e) {
-    var pointerEvent = getPointerEvent(e);
-    currentPosition.x = pointerEvent.pageX;
-    currentPosition.y = pointerEvent.pageY;
-  }
-
-  function tap(target) {
-    alert(target.title);
-  }
-
-  function approximatelyEqual(a, b) {
-    return Math.abs(a - b) < 2;
-  }
-
-  function getPointerEvent(e) {
-    if (e.originalEvent && e.originalEvent.targetTouches) {
-      return e.originalEvent.targetTouches[0];
-    }
-    if (e.targetTouches) {
-      return e.targetTouches[0];
-    }
-    return e;
+  function tap(e) {
+    alert(e.target.title);
   }
 
   return init;
